2fb98bcb917796c47fce244841acd929c4bc6dcb
HUMANIAM <eng.i.saad94@gmail.com>
2019-02-11 16:02:36 -0800

    prime factorization, modular exponentiation, chinese remainder theorem

---
+++ Number_theory/W3/Modular_exponentiation.py
@@ -0,0 +1,97 @@
+"""
+Modular Exponentiation is a fast method to compute b**n % m instead of compute b**n then find b**n %m
+we can find represent n by its binary expansion and start with x = 1 and power = b%m then every time
+update power and update x where x is the final remainder
+"""
+import math
+some_primes = []
+
+# Greatest common divisor using Euclid's algorithm
+def GCD(a, b) :
+	return GCD(b, a%b) if b != 0 else a
+
+# create some primes to find prime factorization of number n
+def create_some_primes(n) :
+	global some_primes
+	for n in range(2, n) :
+		if is_prime(n) : some_primes.append(n)
+
+#prime factorization of number n
+def factors_of_n(n):
+	global some_primes
+	assert n > 1
+	factors = []
+	p = 0
+
+	while not is_prime(n):
+		while n % some_primes[p] != 0 : p += 1
+
+		# reduce n 
+		while n % some_primes[p] == 0 : 
+			factors.append(some_primes[p])
+			n = int(n/some_primes[p])
+
+	if n != 1 : factors.append(n)
+	return factors
+
+# primality of number n
+def is_prime(n):
+	h = math.ceil(n/2)
+	for d in range(2, h) :
+		if n % d == 0 :
+			return False
+
+	return True
+
+# Euler totient counts the number of coprimes of a number
+# a, b are coprime if GCD(a, b) = 1
+def euler_Totient(n) :
+	if n == 1 : return 1
+
+	# if n is prime then coprimes n-1. zero isn't coprime with n where GCD(0, n) = n > 1
+	if is_prime(n) : return n - 1
+	else :
+		# prime factorization
+		prime_factors = factors_of_n(n)
+		coprimes = 1
+		for p in prime_factors :
+			coprimes *= p-1
+
+		return coprimes
+
+"""
+Fermat's little Theorm can be used to make fast modular exponentiation.
+pow(a, p-1) == 1 (mod p) when
+p is prime
+GCD(a, b) = 1 >> a and b are coprime
+so the magic will happen in this case:
+* p is prime and GCD(a, p) = 1 then pow(a, n) (mod p) = pow(a, n % p-1) mod p
+"""
+
+# fast modular of pow(b, e) % m
+def FastModularExponentiation(b, e, m):
+	# if m is prime and GCD(b, m) = 1 use Fermat little theorem
+	if is_prime(m) and GCD(b, m) == 1 :
+		e = e % (m - 1)
+
+	# represent e by binary expansion
+	bits = bin(e)[2:]
+	r = 1
+	power = b%m
+
+	# for every component compute mod m and then multiply it by total remainder
+	for bit in bits[::-1] :	# from right to left
+		if bit == '1' :	r = (r*power) % m
+		power = (power* power) % m
+		
+	return r
+
+# main function	
+def main():
+	create_some_primes(1000)
+	print(FastModularExponentiation(5, 77, 11) == (5**77)%11)
+
+	# print((2**5)%6)
+	# print(FastModularExponentiation(34, 60, 77))
+if __name__ == '__main__':
+	main()
---
+++ Number_theory/W3/chines_reminder.py
@@ -0,0 +1,89 @@
+"""
+Chines Reminder theorm : if GCD(a, b) = 1 then for any reminder Ra, Rb there exist a number n such
+********************** that n%a = Ra and n%b = Rb. How can we find this number.
+n = Ra * Ma * Ya + Rb * M2 * Y2 + ............+ Rk * Mk * Yk
+M = a*b*c*......*z
+Ma = M without a
+Ya = inverse of M without a
+check 
+when we divide n by a all terms contains a except the first one so remainders are zero except the
+first term. from the first term we need Ra as a remainder then (Ma * Ya) % 1 must be 1
+"""
+import math
+from itertools import combinations
+
+def gcd(a, b) :
+	return a if b == 0 else gcd(b, a%b)
+
+def ExtendedEuclid(a, b):
+  assert a >= 0 and b >= 0 
+
+  if b == 0:
+    d, x, y = a, 1, 0
+  else:
+    (d, p, q) = ExtendedEuclid(b, a % b)
+    x = q
+    y = p - q * (a // b)
+
+  assert a % d == 0 and b % d == 0
+  assert d == a * x + b * y
+  return (d, x, y)
+
+""" given set of remainders and set of devisors we will find n that satisfies 
+all this moduler relationships
+"""
+# a and b are coprime if GCD(a, b) = 1
+def relatively_primes(nums) :
+	for p in combinations(range(len(nums)), 2) :
+		if gcd(nums[p[0]], nums[p[1]]) != 1 :
+			return False
+	return True
+
+
+def ChineseRemainderTheorem(remainders, divisors):
+	# all divisors must be coprime where aX + bY = 1 or GCD(a, b) = 1 so d*x is invertable
+	if not relatively_primes(divisors) :
+		return -1     # -1 means they are not relatively prime
+
+	"""
+	m = d1*d2*......dk
+	Mj = m/dj
+	Mj*Yj = 1 mod dj
+	n = r1 * M1 * Y1 + ........... + rk * Mk * Yk
+	"""
+
+	m, Mj, Yj = 1, [], []
+	for d in divisors : m *= d
+	for d in divisors : Mj.append(m/d)
+
+	# compute inverse multiplicative modular of Mj mod dj. Mj and dj are coprime 
+	for i in range(len(divisors)):
+		d, x, y = ExtendedEuclid(Mj[i], divisors[i])
+		Yj.append(x)
+
+	# compute n
+	n = 0
+	for j in range(len(divisors)) :
+		rj = remainders[j]
+		mj = Mj[j]
+		yj = Yj[j]
+		n += rj * mj * yj
+
+	# find n where 0 < n < m
+	if n < 0 :
+		n = (math.ceil(-n/(m)) * m + n) % m
+	else : 
+		n %= m
+
+	# assert that the suggested number satisfies all moduli relations
+	for i in range(len(remainders)) :
+		assert n % divisors[i] == remainders[i]
+
+	return int(n)
+
+def main():
+	print(ChineseRemainderTheorem([2, 3, 2], [3, 5, 7]))
+
+
+if __name__ == '__main__':
+	main()
